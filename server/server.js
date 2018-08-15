fe4cb8d9408a36f1809347eb63d5c32353a4c6ff
omer.sheikh <omer.sheikh@afiniti.com>
2018-08-15 16:02:27 +0500

    PORT env variable added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
 
+
 const publicPath = path.join(__dirname,'../public');
 var app = express();
 
+app.set('PORT',process.env.PORT || 3000)
+
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
+app.listen(app.get('PORT'),()=>{
   console.log('server started on port 3000');
 })
